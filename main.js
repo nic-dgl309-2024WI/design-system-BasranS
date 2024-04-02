00febe716c5df9a82ceb586c1d56175bf32eae68
BasranS <144393592+BasranS@users.noreply.github.com>
2024-04-01 22:10:26 -0700

    Create and add accordion to gift and  guide section

--- main.js
+++ main.js
@@ -71,5 +71,70 @@ document.addEventListener('DOMContentLoaded', function() {
     slides[slideIndex - 1].style.display = "block";
   }
   
-  
-  
+
+  // Accordion
+  function myFunction(btnId, dotsId, moreId) {
+    var dots = document.getElementById(dotsId);
+    var moreText = document.getElementById(moreId);
+    var btnText = document.getElementById(btnId);
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.textContent = "Read On";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.textContent = "Read Less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  // Event listeners for each section
+  document.getElementById("myBtn").addEventListener("click", function() {
+    myFunction("myBtn", "dots", "more");
+  });
+
+  document.getElementById("myBtn1").addEventListener("click", function() {
+    myFunction("myBtn1", "dots1", "more1");
+  });
+
+  document.getElementById("myBtn2").addEventListener("click", function() {
+    myFunction("myBtn2", "dots2", "more2");
+  });
+
+  document.getElementById("myBtn3").addEventListener("click", function() {
+    myFunction("myBtn3", "dots3", "more3");
+  });
+
+
+    // Function to navigate to a specific accordion section
+    function currentSlide(index) {
+      var accordions = document.querySelectorAll('.guide-accordion');
+      for (var i = 0; i < accordions.length; i++) {
+        accordions[i].style.display = "none";
+      }
+      accordions[index].style.display = "block";
+    }
+
+    document.querySelectorAll('.guide-accordion').forEach((section, index) => {
+      if (index === 0) {
+        section.style.display = 'block'; // or any other style you want
+      } else {
+        section.style.display = 'none';
+      }
+    });
+    
+    var acc = document.getElementsByClassName("gift-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
