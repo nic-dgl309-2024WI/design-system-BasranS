63a3035e5f1670635c888962f48c59a49d465c00
BasranS <144393592+BasranS@users.noreply.github.com>
2024-04-01 18:34:30 -0700

    Style the about section of mobile

--- main.js
+++ main.js
@@ -43,9 +43,33 @@ document.addEventListener('DOMContentLoaded', function() {
       showSlide(currentIndex);
     }
   
-    // Set interval to automatically change slide every 2 seconds
+   
     var timer = setInterval(nextSlide, 2000);
   });
   
- 
+
+  // Card Carousel
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("customer-slide");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+  
+  
   
