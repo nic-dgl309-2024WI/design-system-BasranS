c7ffcbd954269445deff64fee8b54a4bad5909d6
BasranS <144393592+BasranS@users.noreply.github.com>
2024-04-15 23:15:19 -0700

    Correct the font awesome link tag

--- design-system.js
+++ design-system.js
@@ -6,10 +6,23 @@ function toggleNav() {
 
 
   function copyToClipboard() {
-    var htmlCode = document.querySelector('.code-container code').innerText;
-    navigator.clipboard.writeText(htmlCode).then(function() {
-      alert('HTML code copied to clipboard!');
-    }, function(err) {
-      console.error('Unable to copy HTML code: ', err);
-    });
-  }
+    // Select the text or content you want to copy
+    var contentToCopy = "Text or content you want to copy";
+
+    // Create a temporary textarea element
+    var tempTextArea = document.createElement("textarea");
+    tempTextArea.value = contentToCopy;
+    document.body.appendChild(tempTextArea);
+
+    // Select the text within the textarea
+    tempTextArea.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextArea);
+
+    // Optionally, you can provide feedback to the user that the text has been copied
+    alert("Text copied to clipboard!");
+}
