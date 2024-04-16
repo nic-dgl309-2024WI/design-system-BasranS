d2e75f50482315dfefcd7b8ede8838c67ddd7f0b
BasranS <144393592+BasranS@users.noreply.github.com>
2024-04-16 13:13:24 -0700

    Style the content of navbar page

--- design-system.js
+++ design-system.js
@@ -6,23 +6,10 @@ function toggleNav() {
 
 
   function copyToClipboard() {
-    // Select the text or content you want to copy
-    var contentToCopy = "Text or content you want to copy";
-
-    // Create a temporary textarea element
-    var tempTextArea = document.createElement("textarea");
-    tempTextArea.value = contentToCopy;
-    document.body.appendChild(tempTextArea);
-
-    // Select the text within the textarea
-    tempTextArea.select();
-
-    // Copy the selected text to the clipboard
-    document.execCommand("copy");
-
-    // Remove the temporary textarea
-    document.body.removeChild(tempTextArea);
-
-    // Optionally, you can provide feedback to the user that the text has been copied
-    alert("Text copied to clipboard!");
-}
+    const textToCopy = "Text to be copied";
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      alert("Text copied to clipboard");
+    }).catch((error) => {
+      console.error("Unable to copy text: ", error);
+    });
+  }
