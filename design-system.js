b7a6ad2b4def03e81a9c60c8d5a0511a28965b70
BasranS <144393592+BasranS@users.noreply.github.com>
2024-04-17 23:48:44 -0700

    Add position classes to spacing and create breadcrumbs

--- design-system.js
+++ design-system.js
@@ -4,4 +4,11 @@ function toggleNav() {
     sideNav.classList.toggle('side-navbar-hidden');
   }
 
+  function copyToClipboard() {
+    const codeBlock = document.querySelector('.code-container code');
+    const textToCopy = codeBlock.textContent;
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => alert("Code copied to clipboard"))
+        .catch(error => console.error("Unable to copy code: ", error));
+}
 
